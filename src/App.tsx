aa89c4308ca7b5074e933cd525a07894fc15e60e
galocastrillon <trader@castrillon.net>
2026-04-09 16:03:03 -0500

    Create App component to display React working message

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1 style={{ fontSize: 32, color: 'black' }}>
+        ✅ REACT ESTÁ FUNCIONANDO
+      </h1>
+    </div>
+  );
+}
