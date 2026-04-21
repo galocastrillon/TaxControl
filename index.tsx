09f92b2e1ed1f5d0beb52124f47fb331440eb2c7
galocastrillon <trader@castrillon.net>
2026-04-21 12:21:25 -0500

    Refactor index.tsx to use ReactDOM.createRoot

--- index.tsx
+++ index.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import { createRoot } from "react-dom/client";
-import App from "./index";
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
 
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root element not found");
-}
-
-createRoot(container).render(<App />);
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
