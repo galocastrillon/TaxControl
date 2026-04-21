d58c22e02fb3eac300d117baf5e425d2aa83242c
galocastrillon <trader@castrillon.net>
2026-04-20 20:51:00 -0500

    Clean up index.tsx by removing unused imports
    Removed commented-out React code for clarity.

--- index.tsx
+++ index.tsx
@@ -1,18 +1,3 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
- para borrar después solo prueba*/
-  
 import { useEffect, useState } from "react";
 
 export default function Home() {
