c7a225df28e814ff24f3917aedfe9be58f9388da
galocastrillon <trader@castrillon.net>
2026-04-20 20:39:54 -0500

    Comment out React imports and rendering code
    Commented out import statements and rendering logic for testing.

--- index.tsx
+++ index.tsx
@@ -1,17 +1,17 @@
-*/import React from 'react';
-*/import ReactDOM from 'react-dom/client';
-*/import App from './App';
+/*import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-*/const rootElement = document.getElementById('root');
-*/if (!rootElement) throw new Error('Failed to find the root element');
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
 
-*/const root = ReactDOM.createRoot(rootElement);
-*/root.render(
-*/  <React.StrictMode>
-*/    <App />
-*/  </React.StrictMode>
-*/);
-*/ para borrar después solo prueba
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ para borrar después solo prueba*/
   
 import { useEffect, useState } from "react";
 
